214ab67832aac7684e76f848c766cbe1e08f0033
Sergey Kobelev <skobelev@competentum.ru>
2017-03-20 18:07:11 +0300

    Init commit

---
+++ Assets/CubeCommands.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions.Comparers;
+
+public class CubeCommands : MonoBehaviour
+{
+    private float speed = 10;
+    private bool manipulationEvent;
+
+    void StartManipulation()
+    {
+        manipulationEvent = true;
+    }
+
+    void Update()
+    {
+        if (manipulationEvent)
+        {
+            var vector = GestureManager.deltaVector;
+            this.transform.position += new Vector3(vector.x*speed,0,0);
+        }
+    }
+}
---
+++ Assets/GestureManager.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.VR.WSA.Input;
+
+public class GestureManager : MonoBehaviour
+{
+    public static RaycastHit HitInfo;
+    public static Vector3 deltaVector;
+
+    public static GestureManager Instance { get; private set; }
+
+    public GameObject FocusedObject { get; private set; }
+
+    GestureRecognizer recognizer;
+
+    void Start()
+    {
+        Instance = this;
+
+        recognizer = new GestureRecognizer();
+        recognizer.SetRecognizableGestures(GestureSettings.ManipulationTranslate | GestureSettings.Tap);
+        recognizer.TappedEvent += (source, tapCount, ray) =>
+        {
+            if (FocusedObject != null)
+            {
+                FocusedObject.SendMessageUpwards("OnSelect");
+            }
+        };
+
+        recognizer.ManipulationStartedEvent += (s, v, r) =>
+        {
+            deltaVector = v;
+            if (FocusedObject != null)
+            {
+                FocusedObject.SendMessageUpwards("StartManipulation");
+            }
+        };
+        recognizer.StartCapturingGestures();
+    }
+
+    void Update()
+    {
+        GameObject oldFocusObject = FocusedObject;
+
+        var headPosition = Camera.main.transform.position;
+        var gazeDirection = Camera.main.transform.forward;
+
+        if (Physics.Raycast(headPosition, gazeDirection, out HitInfo))
+        {
+            FocusedObject = HitInfo.collider.gameObject;
+        }
+        else
+        {
+            FocusedObject = null;
+        }
+
+        if (FocusedObject != oldFocusObject)
+        {
+            recognizer.CancelGestures();
+            recognizer.StartCapturingGestures();
+        }
+    }
+}
