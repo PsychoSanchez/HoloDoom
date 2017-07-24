054b044365a7e50c75809b7a1439bd3d0ca96f34
skobelev <skobelev@competentum.ru>
2017-07-24 14:14:56 +0300

    Common and Cacodemon prefab

---
+++ Assets/Scripts/Common/Common.cs
@@ -0,0 +1,18 @@
+﻿using Assets.Scripts.Monsters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.Common
+{
+    public static class Common
+    {
+        public static T GetObjectScritp<T>(string str)
+        {
+            var tempObject = GameObject.FindGameObjectWithTag(str);
+            return tempObject.GetComponent<T>();
+        } 
+    }
+}
