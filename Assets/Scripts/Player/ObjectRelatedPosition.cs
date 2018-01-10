153ca509a2e78440d6b8150efdf638c1fb19bfda
Aronov Alexander <psychosanchez@outlook.com>
2018-01-10 16:26:32 +0300

    Added object related positioning for ammo box, added gameobject to scene

---
+++ Assets/Scripts/Player/ObjectRelatedPosition.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectRelatedPosition : MonoBehaviour
+{
+    public Transform ParentTransform;
+    public Vector3 MarginFromObject;
+
+    // Use this for initialization
+    void Start()
+    {
+        CheckTransform();
+    }
+
+    private void CheckTransform()
+    {
+        if (ParentTransform == null)
+        {
+            var camera = GameObject.FindGameObjectsWithTag("MainCamera");
+            if (camera == null || camera.Length < 1)
+            {
+                return;
+            }
+
+            ParentTransform = camera[0].transform;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ParentTransform == null)
+        {
+            return;
+        }
+
+        var rotation = Quaternion.LookRotation(ParentTransform.forward, Vector3.up);
+        rotation.x = 0;
+        rotation.z = 0;
+        transform.rotation = rotation;
+        transform.position = ParentTransform.position + transform.forward * MarginFromObject.z - new Vector3(MarginFromObject.x, MarginFromObject.y, 0);
+    }
+}
